b9237b6b7ae81710ddd4e4bce46653a2312f6853
himanshukamal <himanshu.55421@gmail.com>
2024-02-12 13:56:33 +0530

    Updated line svg

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="mt-10 flex justify-center align-center">
-        <LazyLoadImage src="line 5.svg" alt="line" />
+        <LazyLoadImage src="line.svg" alt="line" />
       </div>
       {/* links */}
       <div className="flex space-y-4 flex-col mt-8">
