891a49aa52ce89968aa62f74f0dca93cd9cecbdd
himanshukamal <himanshu.55421@gmail.com>
2024-03-13 17:34:21 +0530

    Updated animated carousel image for patient

--- src/components/invoice/invoiceNavbar.js
+++ src/components/invoice/invoiceNavbar.js
@@ -131,33 +131,19 @@ export default function InvoiceNavbar() {
                   exit={{ opacity: 0, x: "-100%" }}
                   transition={{ duration: 0.6, ease: "easeInOut" }}
                 >
-                  <div className="w-[196px] h-[50px] rounded-full bg-[#F3EDF4] text-center mx-auto flex items-center justify-evenly mt-[40px] shadow-sm">
+                  <div className="px-4 h-[50px] rounded-full bg-[#F3EDF4] text-center mx-auto flex items-center justify-evenly mt-[40px] shadow-sm">
                     <div className="rounded-full w-[30px] h-[30px] bg-[#F3EDF4] drop-shadow-xl grid place-items-center">
                       <LazyLoadImage
-                        src="/HospitalLocation.svg"
+                        src="/icons/accounticon.svg"
                         alt="Hospital Location"
                       />
                     </div>
                     <div className="flex flex-col">
                       <p className="text-[13px] font-bold bg-gradient-to-br from-[#AD37E0] to-[#EE2B3B] text-transparent bg-clip-text">
-                        10000
+                        1.25L
                       </p>
                       <p className="text-[11px] text-black font-bold leading-[9px]">
-                        Doctors
-                      </p>
-                    </div>
-                    <div className="rounded-full w-[30px] h-[30px] bg-[#F3EDF4] drop-shadow-xl grid place-items-center">
-                      <LazyLoadImage
-                        src="/doctoricon.svg"
-                        alt="Hospital Location"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <p className="text-[13px] font-bold bg-gradient-to-br from-[#AD37E0] to-[#EE2B3B] text-transparent bg-clip-text">
-                        60
-                      </p>
-                      <p className="text-[11px] text-black font-bold leading-[9px]">
-                        Clinics
+                        Patients
                       </p>
                     </div>
                   </div>
