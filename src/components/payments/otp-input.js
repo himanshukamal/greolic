7df6f394cf2e6b992eb77a6b8f9720277d380444
Akdex <akmicrowin@outlook.com>
2024-03-14 21:18:00 +0530

    feat: progress bar added

--- src/components/payments/otp-input.js
+++ src/components/payments/otp-input.js
@@ -1,9 +1,30 @@
-import React, { useState, useRef } from "react";
+import { LinearProgress } from "@mui/material";
+import  { useState, useRef, useEffect } from "react";
 
 const OtpInput = () => {
   const [password, setPassword] = useState(Array(4).fill(-1));
   const [activeInput, setActiveInput] = useState(0);
   const inpRefs = useRef(null);
+
+  const [progress, setProgress] = useState(0);
+
+   const [seconds, setSeconds] = useState(30);
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+        setProgress((prevProgress) => prevProgress + 3.33); // Increment by 3.33 for 30 steps
+      } else {
+        clearInterval(countdownInterval);
+        console.log("Countdown complete!");
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [seconds]);
 
   const handleKeyDown = (e, i) => {
     if (e.key === "Backspace") {
@@ -60,6 +81,19 @@ const OtpInput = () => {
           ))}
         </div>
       </form>
+
+      <div className="w-full px-2 my-4">
+        <LinearProgress
+          variant="determinate"
+          value={progress}
+          style={{ backgroundColor: "#fff", color: "#9C527C" }}
+        />
+      </div>
+
+      <div className="flex justify-between w-full mb-4 px-2 ">
+        <p className=" text-[#9C527C] cursor-pointer  ">Resend OTP in</p>
+        <p className=" font-semibold  ">00:{seconds}</p>
+      </div>
     </div>
   );
 };
